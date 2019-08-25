d42b15321296abfeaa822ecf79585e86b0be27e3
Iván Ayala <network.9961@gmail.com>
2019-08-25 15:45:15 -0500

    Añadidas las vistas de login y registro.

--- MI-HOPAC/MI-HOPAC/App.xaml.cs
+++ MI-HOPAC/MI-HOPAC/App.xaml.cs
@@ -13,5 +13,22 @@ namespace MI_HOPAC
     /// </summary>
     public partial class App : Application
     {
+        /* bloque de codigo para cambair el tema de la aplicacopn
+        public ResourceDictionary ThemeDictionary
+        {
+            get { return Resources.MergedDictionaries[0]; }
+        }
+
+        public void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary.MergedDictionaries.Clear();
+            ThemeDictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = uri });
+        }
+        
+        //Se puede invocar tal que:
+        var app = (App)Application.Current;
+        app.ChangeTheme(new Uri("New Uri here"));
+        */
     }
+    
 }
--- MI-HOPAC/MI-HOPAC/Registro.xaml.cs
+++ MI-HOPAC/MI-HOPAC/Registro.xaml.cs
@@ -10,39 +10,34 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 
 namespace MI_HOPAC
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Registro.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Registro : Window
     {
-        public MainWindow()
+        public Registro()
         {
             InitializeComponent();
         }
+
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DragMove();
-        }
-
-        private void Hyperlink_Click(object sender, RoutedEventArgs e)
-        {
-            Registro reg = new Registro();
-            reg.Show();
+            MainWindow inicio = new MainWindow();
+            inicio.Show();
             this.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             if(ValidarInformacion())
             {
@@ -51,26 +46,36 @@ namespace MI_HOPAC
         }
 
         private bool ValidarInformacion()
-        { 
-            /*Que la informacion sea completa*/
-            if (string.IsNullOrEmpty(txt_Contrasena.Password) || string.IsNullOrEmpty(txt_Usuario.Text))
+        {
+            //Que se llenen todos los campos
+            if (   string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtApellido.Text)
+                || string.IsNullOrEmpty(txtCorreo.Text) || string.IsNullOrEmpty(txtContrasena.Password) 
+                || string.IsNullOrEmpty(txtConfirmarContrasena.Password ))
             {
                 MessageBox.Show("Debe completar la información.");
                 return false;
             }
-            /*Que el correo sea correcto*/
-            else if (!Regex.IsMatch(txt_Usuario.Text, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")) //Solo permite el something@something.something
+            //Que el correo sea el correcto
+            else if (!Regex.IsMatch(txtCorreo.Text, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
             {
                 MessageBox.Show("El correo es incorrecto.");
                 return false;
             }
-            /*Que la Contraseña sea correcta*/
-            else if (!Regex.IsMatch(txt_Contrasena.Password, "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?!.*?[¿+_;,.~:´)(}{[|°¬/'#?!@$%^&*-]).{8,}$"))
+            //Que la contraseña sea valida
+            else if (!Regex.IsMatch(txtContrasena.Password, "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?!.*?[¿+_;,.~:´)(}{[|°¬/'#?!@$%^&*-]).{8,}$"))
             {
                 MessageBox.Show("La contraseña deberá contener al menos una mayúscula, una minúscula y un número, deberá contar con al menos 8 caracteres sin hacer uso de caracteres especiales.");
                 return false;
             }
+            //Que las contrsaeñas coincidan
+            else if (txtContrasena.Password != txtConfirmarContrasena.Password)
+            {
+                MessageBox.Show("Las contraseñas no coinciden.");
+                return false;
+            }
             else return true;
         }
+
+        
     }
 }
