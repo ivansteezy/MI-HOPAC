a1fe89d89d86418e0d8d16563729f9d35dddfb7a
LD <watsit777@gmail.com>
2019-09-22 22:08:51 -0500

    Inventarios
    Put a Box

---
+++ MI-HOPAC/MI-HOPAC/InvA.cs
@@ -0,0 +1,8 @@
+﻿namespace MI_HOPAC
+{
+    public class InvA
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
--- MI-HOPAC/MI-HOPAC/Inventario.xaml.cs
+++ MI-HOPAC/MI-HOPAC/Inventario.xaml.cs
@@ -20,9 +20,57 @@ namespace MI_HOPAC
     /// </summary>
     public partial class Inventario : Page
     {
+
+        public List<InvH> ElInventarioH { get; set; }
+
         public Inventario()
         {
             InitializeComponent();
+
+          
+
+            ElInventarioH = new List<InvH>();
+
+            InvH Cosa = new InvH();
+            InvH Cosa2 = new InvH();
+            InvH Cosa3 = new InvH();
+
+            Cosa.Nombre = "Hola";
+            Cosa.Potencia = "20";
+            Cosa.Cantidad = 23;
+
+            ElInventarioH.Add(Cosa);
+
+            Cosa2.Nombre = "asda";
+            Cosa2.Potencia = "87";
+            Cosa2.Cantidad = 15;
+
+            ElInventarioH.Add(Cosa2);
+
+            Cosa3.Nombre = "hjyj";
+            Cosa3.Potencia = "74";
+            Cosa3.Cantidad = 35;
+
+            ElInventarioH.Add(Cosa3);
+
+            DataContext = this;                    
+
         }
+
+        private void Mod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Agr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eli_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
