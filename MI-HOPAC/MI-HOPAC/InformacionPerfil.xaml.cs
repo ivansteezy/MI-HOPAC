6742d54cfe74ad930496ee65d399f37e591d2620
Iván Ayala <network.9961@gmail.com>
2019-10-16 22:59:02 -0500

    Save image.

--- MI-HOPAC/MI-HOPAC/InformacionPerfil.xaml.cs
+++ MI-HOPAC/MI-HOPAC/InformacionPerfil.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using MI_HOPAC.Foundation;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +31,31 @@ namespace MI_HOPAC
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Selecciona una nueva imagen de perfil.";
+
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                        "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                        "Portable Network Graphic (*.png)|*.png";
+
+            if(op.ShowDialog() == true)
+            {
+                string path = op.FileName;
+
+                var bitmap = new Bitmap(path);
+                System.Drawing.Image img = bitmap;
+
+                //Cambiar a directiorio relativo
+                img.Save("C:/Users/Iván/Documents/Mi-Hopac/Mi-Hopac/MI-HOPAC/MI-HOPAC/Resources/ProfilePhoto.png", ImageFormat.Png);
+
+                var bitmapImage = new BitmapImage(new Uri(path));
+                ProfilePhoto.ImageSource = bitmapImage;
+            }
+        }
+
+        void SaveImage()
+        {
+            //Realizar esta tarea en un Thread que no sea de la UI
         }
     }
 }
